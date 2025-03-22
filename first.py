3a0050437617d84d9b298a10c2a815d9380ad0f0
username <talib.siddiqui@students.iiit.ac.in>
2025-03-22 19:00:03 +0530

    Changed first.py, Now I check if the number is greater than 50

--- first.py
+++ first.py
@@ -1,4 +1,11 @@
 import random
 
+def checkNum(num):
+    if num > 50:
+        print("Greater than 50")
+    else:
+        print("Less than 50")
+
+
 myNum = random.randint(1, 100)
-print(myNum)
+checkNum(myNum)
